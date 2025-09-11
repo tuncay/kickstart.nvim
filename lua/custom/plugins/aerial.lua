15bbc49492d88fe33de4a3b4a663b283d9fa2048
Tuncay Baylan <tuncayb@gmail.com>
2025-09-11 18:17:31 +0300

    code outline and vim abolish

---
+++ lua/custom/plugins/aerial.lua
@@ -0,0 +1,10 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  keys = { { '<leader>o', ':AerialOpen<cr>', desc = 'code [o]utline' } },
+  -- Optional dependencies
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+}
