cb4044a8cda585d02a544d0078ac1370eee51d5e
Tuncay Baylan <tuncayb@gmail.com>
2025-09-13 16:03:45 +0300

    keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,4 +1,21 @@
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', 'ı', 'i')
 
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Nop>')
+
+-- Keep things vertically centered during searches
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '*', '*zzzv')
+
+vim.keymap.set('n', '#', '#zzzv')
+vim.keymap.set('n', 'g*', 'g*zzzv')
+vim.keymap.set('n', 'g#', 'g#zzzv')
+
+-- -- Move lines in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 return {}
